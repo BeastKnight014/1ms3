bbb95faaeb44c2b663183a6ccda769101e6f67c5
BeastKnight014 <darkbeastknight014@gmail.com>
2026-06-27 11:26:56 +0530

    Create sh.tf

---
+++ sh.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "public_sg" {
+  name        = "public-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.public_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "All Traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
