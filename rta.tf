e21189ae52e95670cc3646a040c21cc7e2192c99
BeastKnight014 <darkbeastknight014@gmail.com>
2026-06-27 11:26:15 +0530

    Create rta.tf

---
+++ rta.tf
@@ -0,0 +1,6 @@
+resource "aws_route_table_association" "rt_association" {
+  count = length(aws_subnet.public_subnet)
+
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
